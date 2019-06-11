f228d77b3dce6df1acf67c5400597b6d7ce4e320
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-06-11 10:38:25 -0700

    Get route created for notes

--- test/e2e/notes.test.js
+++ test/e2e/notes.test.js
@@ -2,6 +2,13 @@ require('../data-helpers');
 const request = require('supertest');
 const app = require('../../lib/app');
 
+const createNote = note => {
+  return request(app)
+    .post('/api/v1/notes')
+    .send(note)
+    .then(res => res.body);
+};
+
 describe('notes routes', () => {
   it('can create a note via POST', () => {
     return request(app)
@@ -16,4 +23,19 @@ describe('notes routes', () => {
         });
       });
   });
+
+  it('can fetch notes via GET', async() => {
+    const notes = await Promise.all([
+      createNote({ title: 'Hello', body: 'World' }),
+      createNote({ title: 'Goodbye', body: 'Banana' }),
+      createNote({ title: 'UpsideDown', body: 'Smiles' })
+    ]);
+
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        expect(res.body).toEqual(notes);
+      });
+  });
+
 });
